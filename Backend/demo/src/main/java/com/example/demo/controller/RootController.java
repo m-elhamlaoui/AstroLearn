03dcba9c16dbf10543ce65cb6d275e9268060782
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-14 15:23:32 +0100

    After adding WebHook in the github to communicate with jenkins automatically i changed the endpoint root to change

--- Backend/demo/src/main/java/com/example/demo/controller/RootController.java
+++ Backend/demo/src/main/java/com/example/demo/controller/RootController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
+    // This endpoint is used to check if the backend is running
 
     @GetMapping("/")
     public String getRoot() {

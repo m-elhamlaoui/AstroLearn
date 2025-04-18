22f494734dea5f7f143fc94bf34cbd2c51022789
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-18 05:08:19 +0100

    Removed Article Repository methods related to article verification, fixed article repository, added attributes in userDTO

--- Backend/demo/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -11,17 +11,11 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findByAuthorId(Long authorId);
     List<Article> findByTagsName(String tagName);
-    List<Article> findByAuthorIdAndVerifiedTrue(Long authorId);
-    List<Article> findByVerifiedTrue();
+
 
     @Query("SELECT a FROM Article a JOIN a.tags t WHERE t.name IN :tags GROUP BY a HAVING COUNT(t) = :tagCount")
     List<Article> findByAllTags(@Param("tags") List<String> tags, @Param("tagCount") long tagCount);
 
-
-    // Optimizes query: Eagerly fetches 'author' and 'tags' relationships in a single query
-    // to prevent the N+1 select problem when accessing these fields later.
-    @EntityGraph(attributePaths = {"author", "tags"})
-    List<Article> findByVerifiedTrueOrderByCreatedAtDesc();
 }
 
 

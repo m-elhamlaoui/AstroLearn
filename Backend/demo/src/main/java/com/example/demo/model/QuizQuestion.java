229c9d7033123228c847efa7b528c5637a850820
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-20 17:12:30 +0100

    Added CourseServiceImpl , security check to be added later

--- Backend/demo/src/main/java/com/example/demo/model/QuizQuestion.java
+++ Backend/demo/src/main/java/com/example/demo/model/QuizQuestion.java
@@ -1,10 +1,15 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
+
 @Table(name = "quiz_questions")
 public class QuizQuestion {
     @Id

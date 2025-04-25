ee86b8d742035259bf45089fc47c3ec47a2765b0
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-25 15:18:15 +0100

    fixed spring security, signing to include usernamme/email and testes the @PreAuthorize

--- Backend/demo/src/main/java/com/example/demo/controller/ArticleController.java
+++ Backend/demo/src/main/java/com/example/demo/controller/ArticleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/articles")
 @AllArgsConstructor

75455ca75164b913679cede5b68ce7140d067ae0
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-05-08 23:06:44 +0100

    Integrated the courses page with the backend

--- Frontend/lib/axiosInstance.ts
+++ Frontend/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8088', // Default to localhost:8088 if no env var
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8090', // Default to localhost:8088 if no env var
 });
 
 axiosInstance.interceptors.request.use(
